26d92f6cf786f84f5256e682e5bd009dee253358
Sahazid <sahazidhossen@gmail.com>
2026-09-19 21:03:17 +0600

    first commit

--- src/Pages/Layout.jsx
+++ src/Pages/Layout.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import Home from './Home';
 import { Outlet } from 'react-router-dom';
-import NavBar from '../assets/component/NavBar';
-import AnimeInfo from '../assets/component/AnimeInfo';
-
+import NavBar from '../component/NavBar';
+import AnimeInfo from '../component/AnimeInfo';
+// import UserProvider from '../assets/component/Context/userProvider';
 const Layout = () => {
     return (
         <div>
